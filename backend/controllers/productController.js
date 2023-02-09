cf42895cc8d1fa3d7936843126face61a4fc9f72
FransManlangit <fransmanlangit11@gmail.com>
2023-02-09 15:27:37 +0700

    Initial commit

---
+++ backend/controllers/productController.js
@@ -0,0 +1,306 @@
+//* code ni sir */
+// const Product = require('../models/product')
+// const APIFeatures = require("../utils/apiFeatures")
+// //create new product
+
+// exports.newProduct = async(req,res,next) => {
+
+// 	// console.log(req.body);
+
+// 	const product = await Product.create(req.body);
+  
+//     console.log(product)
+//     console.log('from async')
+// 	res.status(201).json({
+// 		success:true,
+// 		product 
+// 	})
+// }
+
+// // exports.getProducts = async (req,res,next) => {
+
+// // 	const products = await Product.find();
+
+// // 	res.status(200).json({
+
+// // 		success: true,
+
+// // 		count: products.length,
+
+// // 		products
+
+// // 	})
+
+// // }
+
+// exports.getProducts = async (req, res, next) => {
+
+// 	const resPerPage = 4;
+
+// 	const productsCount = await Product.countDocuments();
+
+// 	// console.log(productsCount,req.query,Product.find())
+
+// 	// console.log(Product.find())
+
+// 	const apiFeatures = new APIFeatures(Product.find(),req.query).search()
+
+// 	// const products = await Product.find();
+
+// 	// apiFeatures.pagination(resPerPage);
+
+// 	const products = await apiFeatures.query;
+
+	
+
+// 	// console.log(products)
+
+// 	res.status(200).json({
+
+// 		success: true,
+
+// 		count: products.length,
+
+// 		productsCount,
+
+// 		products
+
+// 	})
+
+// }
+
+// exports.getSingleProduct = async(req,res,next) => {
+
+//     const product = await Product.findById(req.params.id);
+
+//     if(!product) {
+
+//             return res.status(404).json({
+//                 success: false,
+//                 message: 'Product not found'
+//             })
+
+//     }
+
+//     res.status(200).json({
+//         success: true,
+//         product
+
+//     })
+
+// }
+// exports.updateProduct = async(req,res,next) => {
+
+// 	let product = await Product.findById(req.params.id);
+// 	if(!product) {
+
+// 	 		return res.status(404).json({
+
+// 	 			success: false,
+
+// 	 			message: 'Product not found'
+// 	 		})
+
+// 	 }
+
+// 	 product = await Product.findByIdAndUpdate(req.params.id,req.body,{
+
+// 	 	new: true,
+// 	 	runValidators:true,
+// 	 	useFindandModify:false
+
+// 	 })
+
+//      console.log(product)
+
+// 	 return res.status(200).json({
+
+// 	 	success:true,
+// 	 	product
+
+// 	 })
+
+// }
+
+// exports.deleteProduct = async(req,res,next) => {
+
+// 	const product = await Product.findById(req.params.id);
+
+// 	if(!product) {
+
+// 	 		return res.status(404).json({
+
+// 	 			success: false,
+
+// 	 			message: 'Product not found'
+
+// 	 		})
+
+// 	 }
+
+// 	 await product.remove();
+
+// 	 res.status(200).json({
+
+// 	 	success: true,
+
+// 	 	message: 'Product deleted'
+
+// 	 })
+
+// }
+
+//* code ni gab */
+
+const Product = require("../models/product");
+const APIFeatures = require("../utils/apiFeatures")
+const mongoose = require("mongoose");
+
+
+//get all products
+// const getProducts = async (req, res, next) => {
+//   const products = await Product.find().sort({ createdAt: -1 });
+//   return res.json({ success: true, count: products.length, products });
+// };
+
+//search product and pagination
+const getProducts = async (req, res, next) => {
+
+	const resPerPage = 4;
+
+	const productsCount = await Product.countDocuments();
+
+	// console.log(productsCount,req.query,Product.find())
+
+	// console.log(Product.find())
+
+	const apiFeatures = new APIFeatures(Product.find(),req.query).search().filter()
+
+	// const products = await Product.find();
+
+	apiFeatures.pagination(resPerPage);
+
+	const products = await apiFeatures.query;
+
+	// console.log(products)
+
+	res.status(200).json({
+		success: true,
+		count: products.length,
+		productsCount,
+		products
+
+	})
+
+}
+
+
+//get single product
+const getSingleProduct = async (req, res, next) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).json({ success: false, message: "Invalid ID" });
+
+  const product = await Product.findById(id);
+
+  if (!product)
+    return res.status(404).json({ success: false, message: "Not found" });
+
+  return res.json({ success: true, product });
+};
+
+
+
+
+
+//create new product
+// const newProduct = async (req, res, next) => {
+//   const requiredFields = [
+//     "name",
+//     "price",
+//     "description",
+//     "ratings",
+//     "images",
+//     "category",
+//     "seller",
+//     "stock",
+//     "numOfReviews",
+//     "reviews",
+//   ];
+//   const emptyFields = requiredFields.filter((field) => !req.body[field]);
+
+//   if (emptyFields.length)
+//     return res
+//       .status(400)
+//       .json({ error: "Please fill all fields", emptyFields });
+
+//   try {
+//     const product = await Product.create(req.body);
+//     return res.json({ success: true, product });
+//   } catch (error) {
+//     return res.status(400).json({ error: error.message });
+//   }
+// };
+
+
+
+
+//create new product
+const newProduct = async (req, res, next) => {
+
+	// console.log(req.body);
+
+	const product = await Product.create(req.body);
+
+	console.log(product)
+	console.log('from async')
+	res.status(201).json({
+		success: true,
+		product
+	})
+}
+
+
+
+//update a product
+const updateProduct = async (req, res, next) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).json({ success: false, message: "Invalid ID" });
+
+  const product = await Product.findOneAndUpdate({ _id: id }, req.body, {
+    new: true,
+  });
+
+  if (!product)
+    return res.status(404).json({ success: false, message: "Not found" });
+
+  return res.json({ success: true, product });
+};
+
+//delete a product
+const deleteProduct = async (req, res, next) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).json({ success: false, message: "Invalid ID" });
+
+  const product = await Product.findOneAndDelete({ _id: id });
+
+  if (!product)
+    return res
+      .status(404)
+      .json({ success: false, message: "Product not found" });
+
+  res.status(200).json({ success: true, message: "Product deleted" });
+};
+
+module.exports = {
+  getProducts,
+  getSingleProduct,
+  newProduct,
+  updateProduct,
+  deleteProduct,
+};
---
+++ backend/routes/product.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const {getProducts,newProduct, getSingleProduct, updateProduct, deleteProduct} = require('../controllers/productController');
+const { isAuthenticatedUser, authorizeRoles} = require('../middlewares/auth');
+
+// router.route('/products').get(getProducts);
+// router.get('/products',getProducts);
+
+router.post('/product/new',newProduct);
+router.get('/product/:id',getSingleProduct);
+router.route('/admin/product/:id').put(updateProduct).delete(deleteProduct);
+
+///WITH AUTH---------------------------------------------
+
+// router.get('/products', isAuthenticatedUser,  getProducts);
+
+router.get('/products',  isAuthenticatedUser,  authorizeRoles('admin'), getProducts)
+router.post('/admin/product/new', isAuthenticatedUser, authorizeRoles('admin'), newProduct);
+router.route('/admin/product/:id').put(isAuthenticatedUser, authorizeRoles('admin'), updateProduct).delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct);
+
+module.exports = router;
